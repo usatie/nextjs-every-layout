5e4564b2d33c8b4a66e651106c90f96432b5c887
Shun Usami <usatie@gmail.com>
2023-11-27 14:31:41 +0900

    [Box] Add box page

---
+++ app/box/page.tsx
@@ -0,0 +1,10 @@
+import { Box } from "../components/box"
+
+export default function BoxPage() {
+    return (
+        <Box padding="p-0" borderWidth="border-0">
+            <Box borderWidth="border-0">Header</Box>
+            <Box borderWidth="border-0">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste laborum reiciendis commodi ullam, quo accusantium nesciunt excepturi ratione. Minus vero fugiat non deserunt totam quod soluta quis, doloribus quisquam iusto fuga provident qui perferendis ad. Dolorum voluptatibus harum ad eos eveniet unde, optio aspernatur cumque autem. Asperiores quae atque doloremque necessitatibus commodi sunt repellat tenetur, eos qui minus earum assumenda praesentium nostrum laboriosam quos vitae odit molestiae corrupti repudiandae, ratione quaerat, cupiditate illum architecto. Veritatis vitae obcaecati ab nesciunt eligendi. Blanditiis numquam soluta itaque voluptatem excepturi maiores laudantium laboriosam quisquam ad, sit dolorem quis voluptate! Minus recusandae optio eius laborum.</Box>
+        </Box>
+    )
+}
