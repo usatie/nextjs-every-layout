02920459605017670cf7e0d9549dc4e156e9f980
Shun Usami <usatie@gmail.com>
2023-11-28 17:11:28 +0900

    Fix build error

--- app/center/page.tsx
+++ app/center/page.tsx
@@ -7,7 +7,7 @@ export default function CenterExamplePage() {
     return (
         <Container>
             <Center>
-                <Stack gap="space-y-6">
+                <Stack space="space-y-6">
                     <h1 className="text-4xl font-bold">Our documentation</h1>
                     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque molestiae optio numquam tenetur quas, sapiente temporibus, ipsa quam, repudiandae velit soluta maxime cupiditate est suscipit ut esse possimus quae? Et ipsam expedita, itaque rerum earum corporis architecto debitis, at nesciunt id harum assumenda. Eligendi quam odio itaque debitis libero. Architecto!</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis deserunt necessitatibus nostrum similique hic eligendi non placeat repellendus ratione et voluptas quas laborum illo sapiente numquam odio est, voluptate magni id? Repudiandae voluptatibus, molestiae iste exercitationem impedit voluptate quisquam eos laudantium doloribus velit itaque adipisci rem corporis vitae sit. Numquam.</p>
